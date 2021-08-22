767d72e138315748c0dee870bb29fe0ff685954b
sathish13898 <sathish13898@gmail.com>
2021-08-22 19:46:14 +0530

    fixed bugs with private key

--- routes/signup.js
+++ routes/signup.js
@@ -8,19 +8,33 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', async (req, res) => {
+  try{
+
     const { error } = validate(req.body); 
     if (error) return res.status(400).send(error.details[0].message);
   
     let user = await User.findOne({ email: req.body.email });
     if (user) return res.status(400).send('User already registered.');
+
+
+    user = new User({
+      username:req.body.username,
+      email:req.body.email,
+      password:req.body.password
+    })
   
-    user = new User(_.pick(req.body, ['name', 'email', 'password']));
+    user = new User(_.pick(req.body, ['username', 'email', 'password']));
     const salt = await bcrypt.genSalt(10);
     user.password = await bcrypt.hash(user.password, salt);
     await user.save();
   
     const token = user.generateAuthToken();
-    res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']+ token));
+    console.log(token);
+    res.header("x-auth-token",token).send(_.pick(user, ['_id', 'username', 'email']));
+  }
+  catch(err){
+    console.log(err);
+  }
   });
   
   module.exports = router; 
